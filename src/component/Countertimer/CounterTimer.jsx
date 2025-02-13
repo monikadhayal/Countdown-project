893362893c0e7ebb40ad051f5c7d26fd38872b69
monikadhayal <monikadhayal25@gmail.com>
2025-02-13 19:19:26 +0530

    Some-other-changement

--- src/component/Countertimer/CounterTimer.jsx
+++ src/component/Countertimer/CounterTimer.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { useState } from "react";
+// //  import React, { useRef, useState } from "react";
+// import { useState } from "react";
+// import { useRef } from "react";
+import React, { useRef, useState ,useEffect } from "react";
 
 export default function CounterTimer() {
-  const[time,setTime] = useState(0);
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false); 
+  const [isPause, setIsPause] = useState(false);
+   
+  const intervalRef = useRef(null);
 
   const handleInput =(event) => {
     setTime(parseInt(event.target.value * 60));
@@ -13,6 +19,37 @@ export default function CounterTimer() {
     const sec = String(time%60).padStart(2,'0');
     return `${min} : ${sec}`
   }
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPause(false);
+  }
+  // const handlePause =() => {
+
+  // }
+  useEffect(()=>{
+       if(isActive && ! isPause && time >0){
+             intervalRef.current = setInterval(()=>{
+              setTime((prev) => prev-1)
+            },1000)
+       }
+       else if(time === 0){
+        clearInterval(intervalRef.current);
+        setIsActive(false);
+        alert('time is up')
+       }
+       return () => clearInterval(intervalRef.current);   
+       } , [isActive,isPause,time])
+
+       const handlePause = () => {
+        setIsPause(!isPause)
+       }
+       const handleReset = () => {
+         clearInterval(intervalRef.current);
+         setIsActive(false);
+         setIsPause(false);
+         setTime(0);
+       }
   return (
     <div className="flex  justify-center items-center h-screen bg-gray-100 text-2xl mb-5 flex-col font-sans">
 
@@ -29,13 +66,21 @@ export default function CounterTimer() {
 
         <div className="flex gap-2.5">
 
-          <button className="bg-blue-500 px-5 py-2 text-base border-none rounded-md">
-            Start
+          <button 
+          onClick={handleStart}
+          disabled = {isActive && !isPause}
+          className="bg-blue-500 px-5 py-2 text-base border-none rounded-md">
+          Start
           </button>
-          <button className="bg-blue-500 px-5 py-2 text-base border-none rounded-md">
-            Pause
+
+          <button 
+          onClick={handlePause} 
+          disabled = {!isActive}
+          className="bg-blue-500 px-5 py-2 text-base border-none rounded-md">
+            {isPause ? 'Resume' : 'Pause'}
           </button>
-          <button className="bg-blue-500 px-5 py-2 text-base border-none rounded-md">
+
+          <button  onClick={handleReset} className="bg-blue-500 px-5 py-2 text-base border-none rounded-md">
             Reset
           </button>
 
